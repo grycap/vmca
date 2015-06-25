969e5a395398e1ca9a0cb3a5ae139ff51711db9e
Carlos de Alfonso <caralla@upv.es>
2015-06-25 15:17:10 +0200

    integrated with the new version of the command line parsing of the cpyutils library

--- vmca.py
+++ vmca.py
@@ -21,7 +21,50 @@ import sys
 import config
 import xmlrpclib
 import logging
-import cpyutils.parameters
+from cpyutils.parameters import *
+
+class VMCACmdLine(CmdLineParser):
+    def __init__(self, proxy, executable, desc, arguments):
+        CmdLineParser.__init__(self, executable, desc = desc, arguments = arguments)
+        self._proxy = proxy
+
+    def getplan(self, result, error):
+        succeed, text = self._proxy.getplan()
+        if succeed:
+            return True, text
+        else:
+            return False, "Could not get the migration plan from VMCA (%s)" % text
+    
+    def forcerun(self, result, error):
+        succeed, text = self._proxy.forcerun()
+        if succeed:
+            return True, text
+        else:
+            return False, "Could not force the run of VMCA (%s)" % text
+    
+    def clean(self, result, error):
+        force = (result.values["-f"])
+        useempty = (result.values["-e"])
+
+        succeed, text = self._proxy.cleanhosts(hosts, force, useempty)
+        if succeed:
+            return True, text
+        else:
+            return False, "Could not clean hosts of VMCA (%s)" % text
+
+    def version(self, result, error):
+        succeed, text = self._proxy.version()
+        if succeed:
+            return True, text
+        else:
+            return False, "Could not get version of VMCA (%s)" % text
+
+    def info(self, result, error):
+        succeed, text = self._proxy.getinfo()
+        if succeed:
+            return True, text
+        else:
+            return False, "Could not get info about VMCA (%s)" % text
 
 def main_function():
     XMLRPC_SERVER = 'http://%s:%s/RPC2' %(config.config_vmca.XMLRPC_HOST, config.config_vmca.XMLRPC_PORT)
@@ -33,138 +76,18 @@ def main_function():
         print "Could not connect to VMCA server %s (please, check if it is running)" % XMLRPC_SERVER, None    
         sys.exit(-1)
 
-        
-    p = cpyutils.parameters.ParameterHandler(['getplan', 'forcerun', 'clean', 'version', 'info'])
-    p.add_flag("-h", "--help")
-    p.add_op_parameter("clean", "-h", "--hosts", None)
-    p.add_op_flag("clean", "-f", "--force")
-    p.add_op_flag("clean", "-u", "--use-empty")
-
-    result, parameters = p.parse(sys.argv[1:])
-    if not result:
-        print "* error parsing parameters\n%s" % "\n".join(parameters.errors)
-        sys.exit(-1)
-
-    if '-h' in parameters.flags:
-        print "usage: "
-        print "\t",p.help(sys.argv[0])
-        sys.exit(0)
-
-    if parameters.operation is None:
-        print "no operation was stated"
-        print "usage: "
-        print "\t",p.help(sys.argv[0])
-        sys.exit()
-        
-    if parameters.operation == 'getplan':
-        succeed, text = proxy.getplan()
-        if succeed:
-            print text
-        else:
-            logging.error("Could not get the migration plan from VMCA (%s)" % text)
-            sys.exit(-1)
-
-    if parameters.operation == 'forcerun':
-        succeed, text = proxy.forcerun()
-        if succeed:
-            print text
-        else:
-            logging.error("Could not force the run of VMCA (%s)" % text)
-            sys.exit(-1)
-
-    if parameters.operation == 'clean':
-        hosts = cpyutils.parameters.break_parameter_into_list(parameters.op_parameters['-h'])
-        if (hosts is None):
-            logging.error("you must stat the hosts that you want to clean using parameter -h")
-            sys.exit(-1)
-            
-        force = ("-f" in parameters.op_flags)
-        useempty = ("-u" in parameters.op_flags)
-
-        succeed, text = proxy.cleanhosts(hosts, force, useempty)
-        if succeed:
-            print text
-        else:
-            logging.error("Could not clean hosts of VMCA (%s)" % text)
-            sys.exit(-1)
-
-    if parameters.operation == 'version':
-        succeed, text = proxy.version()
-        if succeed:
-            print text
-        else:
-            logging.error("Could not get version of VMCA (%s)" % text)
-            sys.exit(-1)
-
-    if parameters.operation == 'info':
-        succeed, text = proxy.getinfo()
-        if succeed:
-            print text
-        else:
-            logging.error("Could not get info about VMCA (%s)" % text)
-            sys.exit(-1)
-
-    sys.exit()
-        
-    operation = (args[0].strip()).lower()
-    parameters = args[1:]
-
-    if operation not in ['getplan', 'force', 'clean', 'cleanforce', 'version']:
-        logging.error("%s is not a valid operation" % operation)
-        sys.exit(-1)
-
-    if operation == 'getplan':
-        if (len(parameters) > 0):
-            logging.error("usage: getplan")
-            sys.exit(-1)
-
-        succeed, text = proxy.getplan()
-        if succeed:
-            print text
-        else:
-            logging.error("Could not get the migration plan from VMCA (%s)" % text)
-            sys.exit(-1)
-
-    if operation == 'force':
-        if (len(parameters) > 0):
-            logging.error("usage: force")
-            sys.exit(-1)
-
-        succeed, text = proxy.forcerun([])
-        if succeed:
-            print text
-        else:
-            logging.error("Could not force the run of VMCA (%s)" % text)
-            sys.exit(-1)
-
-    force = False
-    if operation == 'cleanforce':
-        force = True
-        operation = 'clean'
-
-    if operation == 'clean':
-        if (len(parameters) == 0):
-            logging.error("usage: %s <host name>" % operation)
-            sys.exit(-1)
-
-        succeed, text = proxy.forcerun(parameters, force)
-        if succeed:
-            print text
-        else:
-            logging.error("Could not clean hosts of VMCA (%s)" % text)
-            sys.exit(-1)
-
-    if operation == 'version':
-        if (len(parameters) > 0):
-            logging.error("usage: version")
-            sys.exit(-1)
-
-        succeed, text = proxy.version()
-        if succeed:
-            print text
-        else:
-            logging.error("Could not get version of VMCA (%s)" % text)
-            sys.exit(-1)
+    p = VMCACmdLine(proxy, "vmca", desc = "The VMCA command line utility", arguments = [
+            Operation("getplan", desc = "Gets the current migration plan that is being carried out in the server"),
+            Operation("forcerun", desc = "Forces VMCA to analyze the platform immediately"),
+            Operation("clean", desc = "Migrates all the VMs from one host", arguments = [
+                Argument("node", desc = "Name of the host that is going to be cleaned", mandatory = True, count = 1),
+                Flag("-f", "--force", desc = "Force cleaning even if the host has not its VMs in a stable state"),
+                Flag("-e", "--use-empty", desc = "Use emtpy hosts as a possible destinations")
+            ]),
+            Operation("version", desc = "Gets the version of the VMCA server"),
+            Operation("info", desc = "Gets the monitoring information that has the VMCA server"),
+        ])
+    p.self_service(True)
 
 if __name__ == '__main__':
     main_function()
